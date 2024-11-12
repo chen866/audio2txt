729925494e3229d2ac940c5475de349687d2abcd
chen866 <1617383757@qq.com>
2024-11-13 02:48:38 +0800

    feat: 优化操作

--- audio2txt/ui.py
+++ audio2txt/ui.py
@@ -4,8 +4,7 @@ import time
 from pywebio import start_server
 import threading
 
-from pywebio.output import put_button
-from pywebio.pin import put_textarea, pin, put_file_upload
+from pywebio.pin import put_textarea, pin, put_file_upload, pin_wait_change
 from pywebio_battery import put_audio
 from typing import TYPE_CHECKING
 
@@ -15,11 +14,17 @@ if TYPE_CHECKING:
 
 def ui_function(shared: "Shared"):
     def ui_module():
-        put_file_upload(name="audio", label="Select some audios:", accept="audio/*")
-        put_button("读取选中文件", onclick=lambda: action_upload(shared))
-        put_textarea(name="text2", value="")
+        put_file_upload(
+            name="audio",
+            label="选择一个音频文件，选择后自动识别",
+            accept="audio/*",
+            placeholder="点击选择音频文件",
+        )
+        # put_button("读取选中文件", onclick=lambda: action_upload(shared))
+        pin_wait_change("audio")
+        action_upload(shared)
 
-    start_server(ui_module, port=8988, debug=True)
+    start_server(ui_module, port=8988, debug=True, cdn=False, auto_open_webbrowser=True)
 
 
 def action_upload(shared: "Shared"):
@@ -34,6 +39,7 @@ def action_upload(shared: "Shared"):
             return
         file_content = audio["content"]
         put_audio(file_content)
+        put_textarea(name="text2", value="准备上传", minlength=20)
         # make md5
         md5 = hashlib.md5(file_content).hexdigest()
         obj_key = f"audio/{md5}_{file_name}"
@@ -51,6 +57,7 @@ def action_upload(shared: "Shared"):
         audio_url = shared.tk_oss_client.pre_signed_url(obj_key)
         logging.info(f"audio_url: {audio_url}")
         # upload to tk
+        pin.text2 = "识别中"
         logging.info("start upload to tk")
         res_data = shared.tk_audio_client.upload_audio(audio_url)
         id = res_data["resp"]["id"]
